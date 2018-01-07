37677ca886f35a1763236f7c3e64f89ab6f0b1ea
Awais Hussain <awais@arachnys.com>
2018-01-07 13:24:47 +0000

    Add an if clause to allow users to leave original audio

--- format_video.sh
+++ format_video.sh
@@ -1,3 +1,6 @@
-ffmpeg -i $1 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental "f_vid_"$1
-
-ffmpeg -i "f_vid"$1 -i $2 -shortest -c:v copy -c:a aac -b:a 256k "f_"$1
+if [ -z "$2" ]; then
+    ffmpeg -i $1 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental "f_"$1
+else 
+    ffmpeg -i $1 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental "f_vid_"$1
+    ffmpeg -i "f_vid_"$1 -i $2 -shortest -c:v copy -c:a aac -b:a 256k "f_"$1
+fi
