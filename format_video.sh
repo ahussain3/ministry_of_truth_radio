2ab1006e44b0140f0bc32e69e52270cf6a967fcb
Awais Hussain <awais@arachnys.com>
2018-01-07 13:52:53 +0000

    Combine audio and video in one ffmpeg step

--- format_video.sh
+++ format_video.sh
@@ -1,5 +1,5 @@
 if [ -z "$2" ]; then
     ffmpeg -i $1 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental "f_"$1
 else
-    ffmpeg -i $1 -i $2 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental -map 0:v:0 -map 1:a:0 "f_"$1
+    ffmpeg -i $1 -i $2 -acodec aac -vcodec libx264 -s 1280x720 -r 30 -strict experimental -map 0:v:0 -map 1:a:0 "f_"$1
 fi
