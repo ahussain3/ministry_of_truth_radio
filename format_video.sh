450ef45c88f9d6c8ab6d88fa23c10f506f596d2f
Awais Hussain <awais@arachnys.com>
2018-01-07 13:28:49 +0000

    Do audio overlay and video format in one step

--- format_video.sh
+++ format_video.sh
@@ -1,6 +1,5 @@
 if [ -z "$2" ]; then
     ffmpeg -i $1 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental "f_"$1
-else 
-    ffmpeg -i $1 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental "f_vid_"$1
-    ffmpeg -i "f_vid_"$1 -i $2 -shortest -c:v copy -c:a aac -b:a 256k "f_"$1
+else
+    ffmpeg -i $1 -i $2 -acodec copy -vcodec libx264 -s 1280x720 -r 30 -strict experimental -map 0:v:0 -map 1:a:0 "f_"$1
 fi
